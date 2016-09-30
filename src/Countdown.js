782b378463979e8c9373ecd6fd2b70733cb67637
E. R. C. Pereda <ercp42@gmail.com>
2016-09-30 00:41:10 -0300

    feat(component): Implement countdown basic functionality

--- src/Countdown.js
+++ src/Countdown.js
@@ -3,19 +3,112 @@ import classnames from 'classnames';
 import styles from './Countdown.less';
 
 class Countdown extends React.Component {
-  constructor() {
-    super();
+
+  static toDigit(n) {
+    return n < 10 ? `0${n}` : `${n}`;
+  }
+
+  constructor(props) {
+    super(props);
+
     this.state = {
-      days: '01',
-      hours: '12',
-      mins: '15',
-      secs: '30'
+      days: 0,
+      hours: 0,
+      mins: 0,
+      secs: 0,
+      tick: false
     };
+
+    this.init.bind(this)();
+
+    this.timerInterval = setInterval(this.timer.bind(this), 1000);
+    this.blinkInterval = setInterval(
+        () => this.setState(previousState => ({ tick: !previousState.tick }))
+        , 500);
+  }
+
+  init() {
+    this.alarmOn = new Date();
+    if (this.props.stop) {
+      this.alarmOn = new Date(this.props.stop);
+    }
+    if (this.props.days) {
+      if (typeof this.alarmOn === 'object') {
+        this.alarmOn = this.alarmOn.getTime();
+      }
+      this.alarmOn += this.props.days * 24 * 60 * 60 * 1000;
+    }
+    if (this.props.days) {
+      if (typeof this.alarmOn === 'object') {
+        this.alarmOn = this.alarmOn.getTime();
+      }
+      this.alarmOn += this.props.hours * 60 * 60 * 1000;
+    }
+    if (this.props.minutes) {
+      if (typeof this.alarmOn === 'object') {
+        this.alarmOn = this.alarmOn.getTime();
+      }
+      this.alarmOn += this.props.minutes * 60 * 1000;
+    }
+    if (this.props.secons) {
+      if (typeof this.alarmOn === 'object') {
+        this.alarmOn = this.alarmOn.getTime();
+      }
+      this.alarmOn += this.props.secons * 1000;
+    }
+  }
+
+  timer() {
+    let left;
+
+    left = Math.max(0, Math.floor((this.alarmOn - new Date()) / 1000));
+    const d = Math.floor(left / (24 * 60 * 60));
+    left -= d * 24 * 60 * 60;
+    this.setState({ days: Countdown.toDigit(d) });
+
+    if (d === 0) {
+      // disable days
+    }
+
+    const h = Math.floor(left / (60 * 60));
+    left -= h * 60 * 60;
+    this.setState({ hours: Countdown.toDigit(h) });
+
+    if (d === 0 && h === 0) {
+      // disable hours
+    }
+
+    const m = Math.floor(left / 60);
+    left -= m * 60;
+    this.setState({ mins: Countdown.toDigit(m) });
+
+    if (d === 0 && h === 0 && m === 0) {
+      // disable minutes
+    }
+
+    const s = left;
+    this.setState({ secs: Countdown.toDigit(s) });
+
+    if (this.props.onTick) {
+      this.props.onTick(d, h, m, s);
+    }
+
+    if (d === 0 && h === 0 && m === 0 && s === 0) {
+      if (this.props.onStop) {
+        this.props.onStop();
+      }
+      this.stop();
+    }
+  }
+
+  stop() {
+    clearInterval(this.blinkInterval);
+    clearInterval(this.timerInterval);
   }
 
   render() {
-    const countdownClasses = classnames(styles.countdown);
-    const daysClasses = classnames(styles.part, styles.days);
+    const countdownClasses = classnames(styles.countdown, { [styles.tick]: this.state.tick });
+    const daysClasses = classnames(styles.part, styles.days, );
     const hoursClasses = classnames(styles.part, styles.hours);
     const minutesClasses = classnames(styles.part, styles.minutes);
     const secondsClasses = classnames(styles.part, styles.seconds);
@@ -48,7 +141,20 @@ class Countdown extends React.Component {
 }
 
 Countdown.propTypes = {
-  text: React.PropTypes.string
+  days: React.PropTypes.number,
+  hours: React.PropTypes.number,
+  minutes: React.PropTypes.number,
+  secons: React.PropTypes.number,
+  stop: React.PropTypes.instanceOf(Date),
+  onStop: React.PropTypes.func,
+  onTick: React.PropTypes.func
+};
+
+Countdown.defaultProps = {
+  days: 0,
+  hours: 0,
+  minutes: 0,
+  secons: 0
 };
 
 export default Countdown;
--- src/main.js
+++ src/main.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Countdown from './Countdown';
 
-ReactDOM.render(<Countdown text={'Hello World!!'} />, document.getElementById('app'));
+ReactDOM.render(<Countdown minutes={1} onStop={() => console.log('stoped')} />, document.getElementById('app'));
